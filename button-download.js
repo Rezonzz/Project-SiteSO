5b5e3bd6723a8537ae19a65e8413043f34fa81c3
Rezonzz <brudi225@gmail.com>
2022-03-07 00:47:21 +0000

    SiteSO 1.2v

---
+++ button-download.js
@@ -0,0 +1,11 @@
+const button = document.querySelector(".button");
+                    button.addEventListener("click", () =>{
+                      button.classList.add("active");
+                      setTimeout(()=>{
+                        button.classList.remove("active");
+                        button.querySelector("i").classList.replace("bx-cloud-download", "bx-check-circle")
+                        button.querySelector("span").innerText = "Completed";
+                        let download_href="./Windows-ISO-Downloader.exe";
+                        window.location.href=download_href;
+                      },3000);
+                    });
